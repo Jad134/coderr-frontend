50bc899716460ecaf6466b5335c6c4a92dfbc70a
unknown <jadelnader99@gmail.com>
2025-01-13 16:21:13 +0100

    changeapi links to backend server

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -9,8 +9,8 @@ const GUEST_LOGINS = {
     }
 }
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-const STATIC_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'http://35.222.206.0/api/';
+const STATIC_BASE_URL = 'http://35.222.206.0/';
 
 
 const LOGIN_URL = 'login/';
